83b490c3bc519943fba5b4add961e700ea13653f
Nicholas Jovianto <nicholas.jovianto@gmail.com>
2024-01-17 11:56:21 +0200

    exec9-2

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
 import { Hono } from "https://deno.land/x/hono@v3.7.4/mod.ts";
 import { getFeedback, incrementFeedback } from "./store.js";
+import { Eta } from "https://deno.land/x/eta@v3.1.0/src/index.ts";
+
+const eta = new Eta({ views: `${Deno.cwd()}/templates/` });
 
 const app = new Hono();
 
-app.get("/feedbacks/:value", (c) => {
+app.get("/", async (c) =>  {
+  return c.html(await eta.render("index.eta"));
+});
+
+app.get("/feedbacks/:value", async (c) => {
   const value = Number(c.req.param("value"));
-  if (value >= 1 && value <= 3) {
+  if (value >= 1 && value <= 5) {
     const count = getFeedback(value);
-    return c.text(`Feedback ${value}: ${count}`);
+    const it = {
+      value: value,
+      count: count
+    }
+    return c.html(await eta.render("page.eta", it));
   }
   return c.text("Invalid feedback value", 400);
 });
 
 app.post("/feedbacks/:value", (c) => {
   const value = Number(c.req.param("value"));
-  if (value >= 1 && value <= 3) {
+  if (value >= 1 && value <= 5) {
     incrementFeedback(value);
-    return c.text("Feedback recorded");
+    return c.redirect('/');
   }
   return c.text("Invalid feedback value", 400);
 });
--- store.js
+++ store.js
@@ -1,4 +1,4 @@
-let feedbackCounts = { 1: 0, 2: 0, 3: 0 };
+let feedbackCounts = { 1: 0, 2: 0, 3: 0, 4:0, 5:0 };
 
 function getFeedback(feedbackValue) {
   return feedbackCounts[feedbackValue];
